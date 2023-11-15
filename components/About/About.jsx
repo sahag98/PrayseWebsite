40cefca6465467fc3de840db2baaea05127a2293
Sahak Arzoumanian <arzsahag@gmail.com>
2023-11-14 16:57:20 -0800

    design changes

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,97 +1,99 @@
-"use client"
-import './about.css'
+"use client";
+import "./about.css";
 import Image from "next/image";
-import { motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
 const About = () => {
-
   const about = [
     {
       id: 1,
-      title: 'Folders',
-      image: '/assets/folder.png',
-      desc: 'Create folders to organize different prayers.'
+      title: "Folders",
+      image: "/assets/folder.png",
+      desc: "Create folders to organize different prayers.",
     },
     {
       id: 2,
-      title: 'Devotional',
-      image: '/assets/devotional.png',
-      desc: 'Meditate daily on a devotional provided by @triedbyfire.'
+      title: "Devotional",
+      image: "/assets/devotional.png",
+      desc: "Meditate daily on a devotional provided by @triedbyfire.",
     },
     {
       id: 3,
-      title: 'Community',
-      image: '/assets/earth.png',
-      desc: 'Share prayers and pray for one another through the Community page.'
+      title: "Community",
+      image: "/assets/earth.png",
+      desc: "Share prayers and pray for one another through the Community page.",
     },
     {
       id: 4,
-      title: 'Gospel of Jesus',
-      image: '/assets/book.png',
-      desc: 'Learn about Jesus and take the next step.'
-    }
-  ]
+      title: "Gospel of Jesus",
+      image: "/assets/book.png",
+      desc: "Learn about Jesus and take the next step.",
+    },
+  ];
 
   const items = [
     {
       id: 1,
-      feature: '- Add prayer requests for yourself and others',
+      feature: "- Add prayer requests for yourself and others",
     },
     {
       id: 2,
-      feature: '- Track answered prayers',
+      feature: "- Track answered prayers",
     },
     {
       id: 3,
-      feature: '- Favorite daily verses',
+      feature: "- Favorite daily verses",
     },
     {
       id: 4,
-      feature: '- Meditate on the daily devotional',
+      feature: "- Meditate on the daily devotional",
     },
     {
       id: 5,
-      feature: '- And more!',
+      feature: "- And more!",
     },
-  ]
+  ];
   return (
-    <main id="About" className='about-container'>
-      <section className='about-left'>
+    <main id="About" className="about-container">
+      <section className="about-left">
         {about.map((a) => (
-          <motion.div 
-          initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }} 
-            transition={2} 
-            key={a.id} 
-            className='about-item'>
-            <Image className='items-image' width={200} height={200} src={a.image} alt={a.title + 'image'} />
-            <h2>{a.title}</h2>
-            <p style={{lineHeight:1.5}}>{a.desc}</p>
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={2}
+            key={a.id}
+            className="about-item"
+          >
+            <Image
+              className="items-image"
+              width={200}
+              height={200}
+              src={a.image}
+              alt={a.title + "image"}
+            />
+            <h2 style={{ color: "#212121" }}>{a.title}</h2>
+            <p style={{ color: "#212121", lineHeight: 1.5 }}>{a.desc}</p>
           </motion.div>
         ))}
       </section>
-      <motion.div initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }} className='about-right'>
-        <h2 className='about-title'>Our app offers a user-friendly interface and a range of practical
-          features.</h2>
-        <p className='about-long'>
-          Whether you're looking to deepen your own prayer
-          life or connect with others in your community,
-          our app is a valuable tool for anyone seeking
-          to grow in their faith. Start your journey
-          towards a more intentional and meaningful
-          prayer life today with our prayer list app.
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        className="about-right"
+      >
+        <h2 className="about-title">
+          Our app offers a user-friendly interface and a range of practical
+          features.
+        </h2>
+        <p className="about-long">
+          Whether you're looking to deepen your own prayer life or connect with
+          others in your community, our app is a valuable tool for anyone
+          seeking to grow in their faith. Start your journey towards a more
+          intentional and meaningful prayer life today with our prayer list app.
         </p>
-        <ul>
-          <li>- Add prayer requests for yourself and others</li>
-          <li>- Track answered prayers</li>
-          <li>- Favorite daily verses</li>
-          <li>- Meditate on the daily devotional</li>
-          <li>- And more!</li>
-        </ul>
       </motion.div>
     </main>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- components/Reviews/Reviews.jsx
+++ components/Reviews/Reviews.jsx
@@ -1,76 +1,93 @@
-"use client"
-import { BsPersonCircle } from 'react-icons/bs'
-import './reviews.css'
-import StarRating from './StarRating'
+"use client";
+import { BsPersonCircle } from "react-icons/bs";
+import "./reviews.css";
+import StarRating from "./StarRating";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 const Reviews = () => {
-
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const carouselInfiniteScroll = () => {
     if (currentIndex == reviews.length - 1) {
-      return setCurrentIndex(0)
+      return setCurrentIndex(0);
     }
-    return setCurrentIndex(currentIndex + 1)
-  }
+    return setCurrentIndex(currentIndex + 1);
+  };
 
   useEffect(() => {
-    const interval = setInterval(() => { carouselInfiniteScroll() }, 5000)
+    const interval = setInterval(() => {
+      carouselInfiniteScroll();
+    }, 5000);
 
-    return () => clearInterval(interval)
-  })
+    return () => clearInterval(interval);
+  });
 
   const reviews = [
     {
       id: 1,
-      icon: <BsPersonCircle style={{ marginRight: 10 }} color='#6262ff' size={40} />,
-      name: 'Ricardo Clemente',
-      review: 'Great simple way to keep track of prayer, and good notifications to remind you to take a moment and talk to God.',
-      rating: 5
+      icon: (
+        <BsPersonCircle style={{ marginRight: 10 }} color="#6262ff" size={40} />
+      ),
+      name: "Ricardo Clemente",
+      review:
+        "Great simple way to keep track of prayer, and good notifications to remind you to take a moment and talk to God.",
+      rating: 5,
     },
     {
       id: 2,
-      icon: <BsPersonCircle style={{ marginRight: 10 }} color='#ffa7ff' size={40} />,
-      name: 'Leslie Lopez',
-      review: 'Easy and great and simple. I LOVE IT!',
-      rating: 5
+      icon: (
+        <BsPersonCircle style={{ marginRight: 10 }} color="#ffa7ff" size={40} />
+      ),
+      name: "Leslie Lopez",
+      review: "Easy and great and simple. I LOVE IT!",
+      rating: 5,
     },
     {
       id: 3,
-      icon: <BsPersonCircle style={{ marginRight: 10 }} color='#FFD68A' size={40} />,
-      name: 'Jack Atchabahian',
-      review: 'Great application and easy to use',
-      rating: 5
+      icon: (
+        <BsPersonCircle style={{ marginRight: 10 }} color="#FFD68A" size={40} />
+      ),
+      name: "Jack Atchabahian",
+      review: "Great application and easy to use",
+      rating: 5,
     },
     {
       id: 4,
-      icon: <BsPersonCircle style={{ marginRight: 10 }} color='#add8e6' size={40} />,
-      name: 'Sarona Arzoumanian',
-      review: 'I love this app! It is a cool app where you simply add your prayers. Very straightforward and easy to use!',
-      rating: 5
+      icon: (
+        <BsPersonCircle style={{ marginRight: 10 }} color="#435e66" size={40} />
+      ),
+      name: "Sarona Arzoumanian",
+      review:
+        "I love this app! It is a cool app where you simply add your prayers. Very straightforward and easy to use!",
+      rating: 5,
     },
-  ]
+  ];
   return (
-    <div id='Reviews' className='reviews-container'>
-      <h2>Reviews</h2>
-      <p style={{ marginBottom: 20 }}>See what some of our users have to say about Prayse.</p>
-      <div className='reviews-items'>
+    <div id="Reviews" className="reviews-container">
+      <h2 style={{ color: "#121212" }}>Reviews</h2>
+      <p style={{ marginBottom: 20, color: "#212121" }}>
+        See what some of our users have to say about Prayse.
+      </p>
+      <div className="reviews-items">
         {reviews.map((r) => (
-          <div className='review-item' key={r.id} style={{ transform: `translate(-${currentIndex * 100}%)` }}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div
+            className="review-item"
+            key={r.id}
+            style={{ transform: `translate(-${currentIndex * 100}%)` }}
+          >
+            <div style={{ display: "flex", alignItems: "center" }}>
               {r.icon}
-              <h3>{r.name}</h3>
+              <h3 style={{ color: "#212121" }}>{r.name}</h3>
             </div>
-            <p>{r.review}</p>
+            <p style={{ color: "#212121" }}>{r.review}</p>
             <StarRating index={r.id} rating={r.rating} />
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
